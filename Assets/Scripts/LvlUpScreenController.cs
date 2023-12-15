76f3a43c7d7d710beb6f92b76adaea7c53783887
blebloch <blebloch@GCFRIDF025>
2023-12-15 09:37:27 +0100

    blerg

--- Assets/Scripts/LvlUpScreenController.cs
+++ Assets/Scripts/LvlUpScreenController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class LvlUpScreenController : MonoBehaviour
 {
     void Update()
     {
