aab461a32b79b4717bb69d6a28ebfe6fe0d7d055
blebloch <blebloch@GCFRIDF025>
2023-12-18 09:30:00 +0100

    gameover

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(string scenename)
+    public void SceneLoad(string scenename)
     {
         SceneManager.LoadScene(scenename);
         Time.timeScale = 1.0f;
